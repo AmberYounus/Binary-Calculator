e462c5e295c6082d5112062863489c100fc92aa6
Amber Younus <amberyounus22@gmail.com>
2023-09-05 02:15:53 +0500

    third commit

--- app.js
+++ app.js
@@ -1,31 +1,48 @@
 var screen = "";
+
 function operatorFunx() {
+/// Add Operator
   if (screen.indexOf("+") != -1) {
     var numbers = screen.split("+");
     var x = parseInt(numbers[0], 2);
     var y = parseInt(numbers[1], 2);
     var sum = x + y;
-
     var ans = sum.toString(2);
+///Subtract Operator
   } else if (screen.indexOf("-") != -1) {
     var numbers = screen.split("-");
     var x = parseInt(numbers[0], 2);
     var y = parseInt(numbers[1], 2);
     var sub = x - y;
     var ans = sub.toString(2);
+//Multiplication Operator
   } else if (screen.indexOf("*") != -1) {
     var numbers = screen.split("*");
     var x = parseInt(numbers[0], 2);
     var y = parseInt(numbers[1], 2);
     var mul = x * y;
     var ans = mul.toString(2);
+//Divide operator
   } else if (screen.indexOf("/") != -1) {
     var numbers = screen.split("/");
     var x = parseInt(numbers[0], 2);
     var y = parseInt(numbers[1], 2);
     var div = x / y;
     var ans = div.toString(2);
+
   }
   screen = ans;
+  document.getElementById("res").innerHTML = screen;
+}
+
+function key(c) {
+  // console.log(screen);
+  screen += c;
+  document.getElementById("res").innerHTML = screen;
+}
+
+//clear the screen
+function clr() {
+  screen = "";
   document.getElementById("res").innerHTML = screen;
 }
